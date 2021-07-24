8a169a2fa266df58dc11759a59f5bd1b3f4e6f2e
Bernard Gabon <hewbertgabon@gmail.com>
2021-07-24 16:15:51 +0800

    Update Program-console.cs

--- Program-console.cs
+++ Program-console.cs
@@ -1,22 +1,34 @@
-﻿using System;
+using System;
 using System.Net;
+using System.Threading;
 
-namespace MyNgrok
+class MyNgrok
 {
-	class Program
+	public static void Main(string[] args)
 	{
-		public static void Main(string[] args)
-		{
-			using(var client = new WebClient()) 
+		var isErr = false;
+		var ngrokMsg = "";		
+		do {
+			try 
 			{
-			    string s = client.DownloadString("http://127.0.0.1:4040/api/tunnels");
-			    var forwardUrl = s.Split(':')[4] + ":" + s.Split(':')[5].Split(',')[0];
-			    forwardUrl = forwardUrl.Replace("\"", "");
-			    var apikey = args.Length > 0 ? args[0] : "YOUR_HARDCODED_API_KEY";
-			    string ngrok = client.DownloadString("https://ngrok.bernardgabon.com/?apikey="+ apikey +"&r=" + forwardUrl);
-			    Console.WriteLine(ngrok);			     			   
-			    Console.ReadKey();
+				using (var client = new WebClient())
+				{
+					string s = client.DownloadString("http://127.0.0.1:4040/api/tunnels");
+					var forwardUrl = s.Split(':')[4] + ":" + s.Split(':')[5].Split(',')[0];
+					forwardUrl = forwardUrl.Replace("\"", "");
+					var apikey = args.Length > 0 ? args[0] : "YOUR_HARDCODED_API_KEY";
+					ngrokMsg = client.DownloadString("https://ngrok.bernardgabon.com/?apikey=" + apikey + "&r=" + forwardUrl);
+					isErr = false;
+				}
+			} catch 
+			{
+				Console.WriteLine("Ngrok tunnelling is not yet available...");
+				isErr = true;
+			 	Thread.Sleep(1000);
 			}
-		}
+		} while (isErr);
+			
+		Console.WriteLine(ngrokMsg);			     			   
+		Thread.Sleep(5000);		
 	}
 }
