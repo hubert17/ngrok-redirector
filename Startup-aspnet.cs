11ec1c0257fd16b3a93cc0530ea8d57897165dc6
Bernard Gabon <hewbertgabon@gmail.com>
2021-07-24 18:38:44 +0800

    Update Startup-aspnet.cs

--- Startup-aspnet.cs
+++ Startup-aspnet.cs
@@ -13,32 +13,25 @@ namespace NgrokRedirector
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMemoryCache();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMemoryCache cache)
+        public void Configure(IApplicationBuilder app, IMemoryCache cache)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
-            {                
+            {
+                var redirectHost = "ngrok.bernardgabon.com";
                 var redirect = "redirect";
 
                 endpoints.MapGet("/", async context =>
                 {
                     if (!string.IsNullOrEmpty(context.Request.Query["r"]) && !string.IsNullOrEmpty(context.Request.Query["apikey"]))
                     {
-                        if(context.Request.Query["apikey"].ToString() != "ngrokredirector123")
+                        if(context.Request.Query["apikey"].ToString() != "YOUR_HARDCODED_APIKEY")
                             await context.Response.WriteAsync("Forbidden.");
                         else
                         {
@@ -46,13 +39,18 @@ namespace NgrokRedirector
                             if (!r.StartsWith("http")) r = "https://" + r;
                             var entryOptions = new MemoryCacheEntryOptions().SetPriority(CacheItemPriority.NeverRemove);
                             cache.Set(redirect, r, entryOptions);
-                            await context.Response.WriteAsync("ngrok.bernardgabon.com is now redirected to " + r);
+                            await context.Response.WriteAsync(redirectHost + " is now redirected to " + r);
                         }
                     }
                     else if (cache.Get(redirect) != null)
                         context.Response.Redirect(cache.Get(redirect).ToString());                    
                     else
-                        await context.Response.WriteAsync("Append ?apikey=yourkey&r=forwardingURL to set the redirect. Append /c to clear.");
+                        await context.Response.WriteAsync("Append Redirect Options: \n ?apikey=yourkey&r=forwardingURL to set new redirect \n /s to show current redirect \n /c to clear redirect");
+                });
+
+                endpoints.MapGet("/s", async context =>
+                {
+                    await context.Response.WriteAsync(redirectHost + " is redirecting to " + cache.Get(redirect).ToString());
                 });
 
                 endpoints.MapGet("/c", async context =>
