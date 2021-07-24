670daa974c5892ee31aefc6dee746acb486e4f27
Bernard Gabon <hewbertgabon@gmail.com>
2021-07-25 01:22:15 +0800

    Update ngrok.bat

--- ngrok.bat
+++ ngrok.bat
@@ -1,1 +1,3 @@
-start cmd.exe /c myNgrok YOUR_API_KEY & ngrok authtoken YOUR_NGROK_TOKEN & ngrok http 5501 -host-header="localhost:5501" 
+@echo off
+set /p port="Enter localhost port: "
+start cmd.exe /c myNgrok YOUR_API_KEY & ngrok authtoken YOUR_NGROK_TOKEN & ngrok http %port% -host-header="localhost:%port%" 
