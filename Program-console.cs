1dc2071d1b86f8879a4ab42a51e0783b9199b057
Bernard Gabon <hewbertgabon@gmail.com>
2021-07-25 01:21:02 +0800

    Update Program-console.cs

--- Program-console.cs
+++ Program-console.cs
@@ -12,7 +12,7 @@ class MyNgrok
 		var ngrokMsg = "";
 		
 		Console.WriteLine("Requesting ngrok tunnel public URL...");
-		Thread.Sleep(3000);		
+		Thread.Sleep(2000);		
 		do {
 			try {
 				using (var client = new System.Net.WebClient()) 
@@ -20,10 +20,10 @@ class MyNgrok
 					string s = client.DownloadString("http://127.0.0.1:4040/api/tunnels");
 					var forwardUrl = "https://" + s.Split(':')[5].Split('"')[0].Replace("//", "");
 					// var forwardUrl = s.Split(':')[5] + ":" + s.Split(':')[6].Split(',')[0];
-					var apikey = args.Length > 0 ? args[0] : secret;
+					var apikey = args.Length > 0 && !string.IsNullOrWhiteSpace(args[0]) ? args[0] : secret;
 					ngrokMsg = client.DownloadString(redirectHost + "/?apikey=" + apikey + "&r=" + forwardUrl);
 					Console.WriteLine(ngrokMsg);
-					Thread.Sleep(5000);	
+					Thread.Sleep(3000);	
 					isErr = false;
 				}
 			} catch 
